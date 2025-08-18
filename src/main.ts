deb3967cc10d1a7dad51ffd39459d10768b96eb6
Heath Babb <hbabb@heath-babb.dev>
2025-08-17 20:22:16 -0400

    ✨ feat: completed chapter 6 (#5)
    - add throttling and custom logging functionality

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
+
+// import { MyLoggerService } from './my-logger/my-logger.service';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, {
+//     bufferLogs: true,
+//   });
+//   app.useLogger(app.get(MyLoggerService));
+//   app.enableCors();
+//   app.setGlobalPrefix('api');
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// // eslint-disable-next-line @typescript-eslint/no-floating-promises
+// bootstrap();
